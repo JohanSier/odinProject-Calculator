7674a1edbc3890b380818018fd5054d3f926f4bf
johansierra222 <johansierra222@gmail.com>
2024-07-07 00:49:08 +0000

    feat: Initialize calculator state
    - Initialize calculator state variables: currentInput, previousInput, operator, shouldReset

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// Getting DOM Elements
+const displayPanel = document.getElementById("display")
+
+// Calculator state
+let currentInput = '0';
+let previousInput = '';
+let operator = '';
+let shouldReset = false;
